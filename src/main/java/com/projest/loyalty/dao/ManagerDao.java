1aa4d89085c3f54e03a0c02f582272a50b9f9bb2
Bohdansh <bohdansharipov@gmail.com>
2017-12-02 16:59:12 -0500

    Created managerDao.

---
+++ src/main/java/com/projest/loyalty/dao/ManagerDao.java
@@ -0,0 +1,24 @@
+package com.projest.loyalty.dao;
+
+import com.projest.loyalty.database.Executor;
+import com.projest.loyalty.entity.Manager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ManagerDao {
+
+    private Executor executor;
+
+    public ManagerDao(Connection connection) {
+        this.executor = new Executor(connection);
+    }
+
+    public Manager get(long id) throws SQLException {
+        return executor.execQuery(String.format("select * from Manager where manager_id=%d", id), result -> {
+            result.next();
+            return new Manager(result.getLong(1), result.getString(2),
+                    result.getString(3), result.getString(4), result.getString(5));
+        });
+    }
+}
