94d87d769754b37f53903a5333a1c793fef2d014
Bohdansh <bohdansharipov@gmail.com>
2017-12-03 00:38:20 -0500

    Updated manager banner; Created controller for manager getting loyalty program info.

--- src/main/java/com/projest/loyalty/dao/LoyaltyProgramDAO.java
+++ src/main/java/com/projest/loyalty/dao/LoyaltyProgramDAO.java
@@ -1,25 +1,33 @@
 package com.projest.loyalty.dao;
 
+import com.projest.loyalty.controllers.LoyaltyProgramsController;
 import com.projest.loyalty.database.DBService;
 import com.projest.loyalty.database.Executor;
 import com.projest.loyalty.entity.Company;
 import com.projest.loyalty.entity.LoyaltyProgram;
+import org.apache.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class LoyaltyProgramDAO {
+    private static final Logger logger = Logger.getLogger(LoyaltyProgramsController.class);
     private Executor executor;
 
     public LoyaltyProgramDAO(Connection connection) {
         this.executor = new Executor(connection);
     }
 
-    public LoyaltyProgram get(long id) throws SQLException {
-        return executor.execQuery(String.format("select * from LoyaltyProgram where loyalty_id=%d", id), result -> {
-            result.next();
-            return new LoyaltyProgram(result.getLong(1), result.getString(2),
-                    new CompanyDAO(DBService.getMysqlConnection()).get(result.getLong(3)));
-        });
+    public LoyaltyProgram get(long id) {
+        try {
+            return executor.execQuery(String.format("select * from LoyaltyProgram where loyalty_id=%d", id), result -> {
+                result.next();
+                return new LoyaltyProgram(result.getLong(1), result.getString(2),
+                        new CompanyDAO(DBService.getMysqlConnection()).get(result.getLong(3)));
+            });
+        } catch (SQLException e) {
+            logger.error("Could not execute query", e);
+        }
+        return null;
     }
 }
