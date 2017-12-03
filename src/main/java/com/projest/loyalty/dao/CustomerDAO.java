7c1667315c062288a4670c67bfe127117baefb9b
alexulyanov <alex.ulyanov90@gmail.com>
2017-12-03 03:23:48 -0500

    Added basic login functionality.

--- src/main/java/com/projest/loyalty/dao/CustomerDAO.java
+++ src/main/java/com/projest/loyalty/dao/CustomerDAO.java
@@ -23,4 +23,18 @@ public class CustomerDAO {
         });
     }
 
+    public Customer getCustomerByLogin(String login) {
+        try {
+            return executor.execQuery(String.format("select * from customer where customer_login='%s'", login), result -> {
+                result.next();
+                return new Customer(result.getLong(1), result.getString(2),
+                        result.getString(3), result.getString(4), result.getString(5),
+                        result.getString(6));
+            });
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
