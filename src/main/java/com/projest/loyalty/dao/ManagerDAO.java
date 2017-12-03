dbbe8b56bce698e68f18243cf142dcdb2184d67e
Bohdansh <bohdansharipov@gmail.com>
2017-12-02 20:34:22 -0500

    Created query for updates and deletes. Added methods to the managerDao. Added dependencies.

--- src/main/java/com/projest/loyalty/dao/ManagerDAO.java
+++ src/main/java/com/projest/loyalty/dao/ManagerDAO.java
@@ -21,4 +21,24 @@ public class ManagerDAO {
                     result.getString(3), result.getString(4), result.getString(5));
         });
     }
+
+    public boolean checkExistsByEmail(String email) throws SQLException {
+        Manager manager = executor.execQuery(String.format("select * from Manager where manager_email=%s", email), result -> {
+            result.next();
+            return new Manager(result.getLong(1), result.getString(2),
+                    result.getString(3), result.getString(4), result.getString(5));
+        });
+        return manager != null;
+    }
+
+    public boolean insertManager(String givenName, String familyName, String email, String phoneNumber) {
+        try {
+            executor.execQuery(String.format("INSERT INTO Manager (manager_first_name, manager_last_name, " +
+                    "manager_email, manager_phone_numb)" +
+                    "VALUES (%s, %s, %s, %s);", givenName, familyName, email, phoneNumber));
+        } catch (SQLException e) {
+            return false;
+        }
+        return true;
+    }
 }
