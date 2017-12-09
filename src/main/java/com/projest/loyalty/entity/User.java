1f1e1166ff4098d9776162326514fe745f91173a
Bohdansh <bohdansharipov@gmail.com>
2017-12-09 16:18:28 -0500

    Set login unique

--- src/main/java/com/projest/loyalty/entity/User.java
+++ src/main/java/com/projest/loyalty/entity/User.java
@@ -11,6 +11,8 @@ public class User {
     private long id;
     private String name;
     private String surname;
+
+    @Column(unique = true)
     private String login;
     private String password;
     private UserRole userRole;
