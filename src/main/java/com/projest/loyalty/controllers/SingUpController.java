d3c1d920834518fd87977e3b031d34c4f27d2c74
Bohdansh <bohdansharipov@gmail.com>
2017-12-30 15:42:10 -0500

    Adjusted singup controller.

--- src/main/java/com/projest/loyalty/controllers/SingUpController.java
+++ src/main/java/com/projest/loyalty/controllers/SingUpController.java
@@ -2,12 +2,23 @@ package com.projest.loyalty.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class SingUpController {
 
-    @RequestMapping("/signup")
+    @RequestMapping("/addUser")
     public String signup() {
-        return "signup";
+        return "singup";
+    }
+
+    // TODO Implement adding
+    @RequestMapping(value = "/singUpCustomer", method = RequestMethod.POST)
+    public String submit(@RequestParam("login") String login, @RequestParam("password") String password,
+                         Map<String, Object> model) {
+        return "adminview";
     }
 }
