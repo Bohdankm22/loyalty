2799cee3c85932dcdd7122a5e2fb3aabcada9c79
Bohdansh <bohdansharipov@gmail.com>
2017-12-30 16:47:43 -0500

    Added getting tasks list for managers.

--- src/main/java/com/projest/loyalty/controllers/ManagerController.java
+++ src/main/java/com/projest/loyalty/controllers/ManagerController.java
@@ -1,28 +1,26 @@
 package com.projest.loyalty.controllers;
 
-import com.projest.loyalty.entity.User;
-import com.projest.loyalty.entity.UserRole;
-import com.projest.loyalty.repository.UserRepository;
+import com.projest.loyalty.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class ManagerController {
 
-    private final UserRepository userRepository;
+    private final TaskRepository taskRepository;
 
     @Autowired
-    public ManagerController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public ManagerController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
     }
 
     @RequestMapping(value = "/viewtasks", method = RequestMethod.GET)
     public String submit(Map<String, Object> model) {
+        model.put("tasks", taskRepository.findAll());
         return "managerviewtasks";
     }
 }
