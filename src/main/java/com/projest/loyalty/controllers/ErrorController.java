32c1f9496893a73e1b6bcbb7948ec9c6b2ed8eed
Bohdansh <bohdansharipov@gmail.com>
2017-12-30 16:22:31 -0500

    Added controller anno for error.

--- src/main/java/com/projest/loyalty/controllers/ErrorController.java
+++ src/main/java/com/projest/loyalty/controllers/ErrorController.java
@@ -1,7 +1,9 @@
 package com.projest.loyalty.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class ErrorController {
 
     @RequestMapping("/error")
