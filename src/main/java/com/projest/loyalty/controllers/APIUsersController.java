6a3aa0b310f4b4c2a0b5521fc3256aa832540c34
Bohdansh <bohdansharipov@gmail.com>
2017-12-30 14:39:33 -0500

    Created few API calls.

--- src/main/java/com/projest/loyalty/controllers/APIUsersController.java
+++ src/main/java/com/projest/loyalty/controllers/APIUsersController.java
@@ -1,7 +1,9 @@
 package com.projest.loyalty.controllers;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.projest.loyalty.entity.User;
 import com.projest.loyalty.entity.UserRole;
+import com.projest.loyalty.entity.view.Views;
 import com.projest.loyalty.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -32,13 +34,27 @@ public class APIUsersController {
         return String.format("Saved user %s", user);
     }
 
+    @JsonView(Views.Users.class)
     @RequestMapping(path = "/user/", method = RequestMethod.GET)
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
 
+    @JsonView(Views.User.class)
     @RequestMapping(path = "/user/{id}", method = RequestMethod.GET)
     public User getUser(@PathVariable("id") long id) {
+        return userRepository.findOne(id);
+    }
+
+    @JsonView(Views.Task.class)
+    @RequestMapping(path = "/user/{id}/task", method = RequestMethod.GET)
+    public User getUserTasks(@PathVariable("id") long id) {
+        return userRepository.findOne(id);
+    }
+
+    @JsonView(Views.Task.class)
+    @RequestMapping(path = "/user/{id}/task/{taskid}", method = RequestMethod.GET)
+    public User getUserTask(@PathVariable("id") long id, @PathVariable("taskid") long taskId) {
         return userRepository.findOne(id);
     }
 }
