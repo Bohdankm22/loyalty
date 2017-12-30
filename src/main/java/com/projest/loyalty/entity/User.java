7f49682e27d01597f68d999029cc678b05e5d55e
Bohdansh <bohdansharipov@gmail.com>
2017-12-30 12:39:42 -0500

    Adjusted user entity id column name to use it as foreighn key.

--- src/main/java/com/projest/loyalty/entity/User.java
+++ src/main/java/com/projest/loyalty/entity/User.java
@@ -8,6 +8,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
     private long id;
     private String name;
     private String surname;
