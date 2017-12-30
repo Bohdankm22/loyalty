a3d68ebf3bc59c2f17e632b1f35807747f36c7d6
Bohdansh <bohdansharipov@gmail.com>
2017-12-30 18:12:38 -0500

    Putting some data in session.

--- src/main/java/com/projest/loyalty/controllers/LogInController.java
+++ src/main/java/com/projest/loyalty/controllers/LogInController.java
@@ -2,14 +2,15 @@ package com.projest.loyalty.controllers;
 
 import com.projest.loyalty.entity.User;
 import com.projest.loyalty.entity.UserRole;
-import com.projest.loyalty.repository.TaskRepository;
 import com.projest.loyalty.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @Controller
@@ -29,14 +30,16 @@ public class LogInController {
 
     @RequestMapping(value = "/loginUser", method = RequestMethod.POST)
     public String submit(@RequestParam("login") String login, @RequestParam("password") String password,
-                         Map<String, Object> model) {
+                         Map<String, Object> model, HttpSession session) {
         User user = userRepository.findByLoginPassword(login, password);
         if (user == null) {
             return "/error";
         }
         String result = "/error";
-        model.put("user", user);
-        model.put("login", user.getName() + " " + user.getSurname());
+        session.setAttribute("user", user.getId());
+
+        model.put("username", user);
+        session.setAttribute("login", user.getName() + " " + user.getSurname());
         switch (user.getUserRole()) {
             case HR:
                 result = "hrview";
