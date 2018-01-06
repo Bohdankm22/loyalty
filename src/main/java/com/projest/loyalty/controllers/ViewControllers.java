b4c5c21402c045f6dd441d491028662c61de3549
Bohdansh <bohdansharipov@gmail.com>
2018-01-06 16:46:18 -0500

    Added views controllers.

---
+++ src/main/java/com/projest/loyalty/controllers/ViewControllers.java
@@ -0,0 +1,56 @@
+package com.projest.loyalty.controllers;
+
+import com.projest.loyalty.entity.User;
+import com.projest.loyalty.entity.UserRole;
+import com.projest.loyalty.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+@Controller
+public class ViewControllers {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public ViewControllers(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping("/managerview")
+    public String managv(Map<String, Object> model, HttpSession session) {
+        model.put("employees", userRepository.findByRole(UserRole.EMPLOYEE.ordinal()));
+        return "managerview";
+    }
+
+    @RequestMapping(value = "/adminview")
+    public String adminv(Map<String, Object> model, HttpSession session, HttpServletRequest request) {
+        model.put("users", userRepository.findAll());
+        return "adminview";
+    }
+
+    @RequestMapping(value = "/employeeview")
+    public String empv(Map<String, Object> model, HttpSession session, HttpServletRequest request) {
+        long userid = (long)session.getAttribute("user");
+        User user = userRepository.findOne(userid);
+        model.put("tasks", user.getTasks());
+        return "employeeview";
+    }
+
+    @RequestMapping(value = "/hrview")
+    public String hrv(Map<String, Object> model, HttpSession session, HttpServletRequest request) {
+        return "hrview";
+    }
+
+    @RequestMapping(value = "/accountantview")
+    public String accv(Map<String, Object> model, HttpSession session, HttpServletRequest request) {
+        model.put("emp", userRepository.findAll());
+        return "accountantview";
+    }
+}
